304bc76fd04b3377010884d52a8fb169cbe3bf96
AliciaNazar <alicianazarab@gmail.com>
2025-01-30 23:50:10 -0300

    refactorizando sprint5

--- src/main/java/com/mindhub/api_gateway/config/GatewayConfig.java
+++ src/main/java/com/mindhub/api_gateway/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package com.mindhub.api_gateway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,31 +10,39 @@ import org.springframework.http.HttpMethod;
 @Configuration
 public class GatewayConfig {
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    private AdminFilter adminFilter;
+
     @Bean
     public RouteLocator customRouter(RouteLocatorBuilder routeLocatorBuilder) {
 
         return routeLocatorBuilder.routes()
-                .route("user-microservice",r -> r.path("/api/users/**")
-                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                .route("user-microservice", r -> r.path("/api/auth/**")
                         .uri("lb://user-microservice"))
-                .route("product-microservice",r -> r.path("/api/products/**")
+                .route("user-microservice",r -> r.path("/api/users/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
+                        .uri("lb://user-microservice"))
+                .route("user-microservice",r -> r.path("/api/admin/**")
+                        .filters(f -> f.filters(jwtAuthenticationFilter,adminFilter))
+                        .uri("lb://user-microservice"))
+                .route("product-microservice",r -> r.path("/api/products/public/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
                         .uri("lb://product-microservice"))
-                .route("order-microservice",r -> r.path("/api/orders/**")
+                .route("product-microservice",r -> r.path("/api/products/admin/**")
+                        .filters(f -> f.filters(jwtAuthenticationFilter,adminFilter))
+                        .uri("lb://product-microservice"))
+                .route("order-microservice",r -> r.path("/api/orders/user/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
                         .uri("lb://order-microservice"))
-                .route("delete-product", r -> r.path("/api/products/{id}")
-                        .and().method(HttpMethod.DELETE)
-                        .and().header("Role", "ADMIN")
-                        //.filters(f -> f.addRequestHeader("Authorization", "Bearer tokenDelAdmin"))  //aquí podría poner el token de admin cuando trabaje con eso
-                        .uri("lb://product-microservice"))  // redirijo ahí
+                .route("order-microservice",r -> r.path("/api/orders/admin/**")
+                        .filters(f -> f.filters(jwtAuthenticationFilter,adminFilter))
+                        .uri("lb://order-microservice"))
+                .route("orderItem-service", r->r.path("/api/order-items/user/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
+                        .uri("lb://order-service"))
                 .build();
     }
 }
-
-//Esto era para comprobar si se estaba añadiendo al header lo que yo quería
-//                        .filters(f -> f
-//                                .addRequestHeader("Hello", "World")
-//                                .filter((exchange, chain) -> {
-//                                    // Log the request headers
-//                                    System.out.println("Request Headers: " + exchange.getRequest().getHeaders());
-//                                    return chain.filter(exchange);
-//                                }))
---
+++ src/main/java/com/mindhub/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.mindhub.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+// TUVE QUE CAMBIAR ESTAS DOS Y PONERLE REACTIVE DESPUES DE CORS:
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange( authorizeExchangeSpec ->
+                        authorizeExchangeSpec.anyExchange().permitAll())
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                //.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .build();
+    }
+
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("*")); // Permite cualquier origen
+//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+//        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+}
